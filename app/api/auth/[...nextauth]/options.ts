7cbee95e4875518035a1168615661ce575edbc89
ratishjain <ratishjain6@gmail.com>
2024-05-06 19:36:27 +0530

    user now getting stored in db after authentication

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/util/db";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -9,4 +9,47 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ account, profile }: any) {
+      if (account.provider === "google") {
+        try {
+          const user = await prisma.user.findUnique({
+            where: {
+              email: profile.email,
+            },
+            select: {
+              id: true,
+              image: true,
+            },
+          });
+
+          if (!user) {
+            const userCreation = await prisma.user.create({
+              data: {
+                email: profile.email,
+                image: profile.picture,
+              },
+            });
+
+            if (userCreation) return true;
+          } else {
+            if (user.image != profile.picture) {
+              await prisma.user.update({
+                data: {
+                  image: user.image,
+                },
+                where: {
+                  email: profile.email,
+                },
+              });
+            }
+            return true;
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      return false;
+    },
+  },
 };
