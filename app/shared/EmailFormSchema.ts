c8a03a3897ccf4c0f59c4e796fd135bec2736850
kaushal1717 <kpadaliya1717@gmail.com>
2024-07-04 10:12:43 +0530

    commmit

---
+++ app/shared/EmailFormSchema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const emailFormSchema = z.object({
+  emailPurspose: z.enum([
+    "Follow up",
+    "Job Applications",
+    "To CEO/founder",
+    "Referrals",
+    "Product Promotion",
+  ]),
+  subject: z.string().min(1, "Subject is required"),
+  emailTone: z.enum(["Shorter", "Longer", "Simpler", "Casual", "Professional"]),
+  socialLinks: z
+    .array(
+      z.object({
+        platform: z.string().min(1, "Platform is required"),
+        link: z.string().url("Must be valid URL"),
+      })
+    )
+    .max(4, "You can only add upto 4 social links"),
+});
+
+export type emailFormType = z.infer<typeof emailFormSchema>;
---
+++ components/component/email-create/MessageTones.tsx
@@ -0,0 +1,51 @@
+import { z } from "zod";
+import {
+  useForm,
+  useFieldArray,
+  SubmitHandler,
+  useFormContext,
+} from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { emailFormType } from "@/app/shared/EmailFormSchema";
+
+function MessageTonesSection() {
+  const emailTones = ["Shorter", "Longer", "Professional", "Casual", "Simple"];
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext<emailFormType>();
+  const emailTone = watch("emailTone");
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-3">Email Tone</h2>
+      <div className="grid grid-cols-3 sm:grid-cols-5 gap-2">
+        {emailTones.map((tone) => (
+          <label
+            key={tone}
+            className={
+              emailTone === tone
+                ? "border border-gray-200 flex justify-center items-center cursor-pointer bg-primary text-sm rounded-full px-4 py-2 font-semibold text-gray-600"
+                : "border border-gray-200 flex justify-center items-center cursor-pointer bg-seconday text-sm rounded-full px-4 py-2 font-semibold "
+            }
+          >
+            <input
+              type="radio"
+              value={tone}
+              {...register("emailTone", { required: "This field is required" })}
+              className="hidden"
+            />
+            <span>{tone}</span>
+          </label>
+        ))}
+      </div>
+      {errors.emailTone && (
+        <span className="text-red-500 text-sm font-bold">
+          {errors.emailTone?.message}
+        </span>
+      )}
+    </div>
+  );
+}
+
+export default MessageTonesSection;
