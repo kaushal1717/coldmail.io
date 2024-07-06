47f3c0381d4fb1f106f20ba853e591ebd1cf4d15
kaushal1717 <kpadaliya1717@gmail.com>
2024-07-06 15:59:09 +0530

    feat: email generation

--- app/shared/EmailFormSchema.ts
+++ app/shared/EmailFormSchema.ts
@@ -2,18 +2,18 @@ import { z } from "zod";
 
 export const emailFormSchema = z.object({
   emailPurpose: z.enum([
-    "Follow up",
-    "Job Applications",
-    "To CEO/founder",
-    "Referrals",
-    "Product Promotion",
+    "follow-up",
+    "job-application",
+    "to-ceo",
+    "referrals",
+    "product-promotion",
   ]),
   subject: z.string().min(1, "Subject is required"),
   emailTone: z.enum(["Shorter", "Longer", "Simple", "Casual", "Professional"]),
   socialLinks: z
     .array(
       z.object({
-        platform: z.string().min(1, "Platform is required"),
+        platForm: z.string().min(1, "Platform is required"),
         link: z.string().url("Must be valid URL"),
       })
     )
