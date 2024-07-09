3af142340f31fa8341dc21e6ed4c9dffcea3a638
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-07-09 22:28:21 +0530

    templates saved in db

---
+++ actions/actions.ts
@@ -0,0 +1,37 @@
+"use server";
+import {
+  authOptions,
+  CustomSession,
+} from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import { prisma } from "@/util/db";
+
+export const handleSave = async (
+  response: string,
+  subject: string,
+  category: string
+) => {
+  const session: CustomSession | null = await getServerSession(authOptions);
+  try {
+    const email = await prisma.email.create({
+      data: {
+        authorId: session?.user?.id!,
+        content: response,
+        subject: subject,
+        category: category,
+      },
+    });
+
+    const user = await prisma.user.update({
+      where: {
+        userId: session?.user?.id!,
+      },
+      data: {
+        totalEmails: { increment: 1 },
+      },
+    });
+    return email;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,18 @@
 import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "@/util/db";
+import { ISODateString, Session, User } from "next-auth";
+import { JWT } from "next-auth/jwt";
+export interface CustomSession {
+  user?: CustomUser;
+  expires: ISODateString;
+}
+
+export interface CustomUser {
+  id?: string | null;
+  name?: string | null;
+  email?: string | null;
+  image?: string | null;
+}
 
 export const authOptions = {
   providers: [
@@ -10,6 +23,27 @@ export const authOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
+    async jwt({ token, user }: any) {
+      // Persist the OAuth access_token and or the user id to the token right after signin
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+    session: async ({
+      session,
+      user,
+      token,
+    }: {
+      session: CustomSession;
+      user: User;
+      token: JWT;
+    }) => {
+      if (session?.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
     async signIn({ account, profile }: any) {
       if (account.provider === "google") {
         try {
@@ -18,7 +52,7 @@ export const authOptions = {
               email: profile.email,
             },
             select: {
-              id: true,
+              userId: true,
               image: true,
             },
           });
@@ -28,6 +62,7 @@ export const authOptions = {
               data: {
                 email: profile.email,
                 image: profile.picture,
+                userId: profile.sub,
               },
             });
 
