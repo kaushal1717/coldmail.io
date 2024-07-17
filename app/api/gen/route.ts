d7606f56ff4b645f042bda0794195857e9b71bc2
kaushal1717 <kpadaliya1717@gmail.com>
2024-07-17 21:55:13 +0530

    tried impoving prompt

--- app/api/gen/route.ts
+++ app/api/gen/route.ts
@@ -11,11 +11,26 @@ export async function POST(request: Request) {
     socialLinks,
     skills,
   }: emailFormType = await request.json();
-  let links: string = "";
-  socialLinks.forEach((element) => {
-    links += element + ",";
-  });
-  let promptString = `generate an email by ${senderName} for ${emailPurpose} with subject as ${subject} with the skills in ${skills}  and set the tone of email as ${emailTone}, include social links -${links} make sure the length is concise and not lengthy, please generate only body of the email without any final notes or anything`;
+
+  let links: string = socialLinks
+    .map((socialLink) => `${socialLink.platform}: ${socialLink.link}`)
+    .join(", ");
+
+  let givenFactors: string[] = [
+    "1. Generate only body of email",
+    `2. DO NOT include ANY introductory phrases such as "Here is the email:", "The email is as follows:", "Here's the generated email:", or ANY similar variations. Start directly with the email content`,
+    "3. DO NOT include the subject line",
+    "4. Start email with salutation dear xyz or respected xyz",
+    `5. In the closing paragraph, before "Sincerely," ${
+      socialLinks && socialLinks.length > 0
+        ? `include this exact text: "You can reach me at the following: ${links}"`
+        : "do NOT mention any social links."
+    }.`,
+  ];
+
+  let factors: string = givenFactors.map((factor) => `${factor}`).join("\n");
+
+  let promptString = `generate an email by ${senderName} for ${emailPurpose} for ${subject} with the skills in ${skills} and set the tone of email as ${emailTone}, and strictly consider following factors: \n ${factors}. \nPlease take care of all these factors while generating mail`;
   const generator = await groq.chat.completions.create({
     messages: [
       {
@@ -23,7 +38,8 @@ export async function POST(request: Request) {
         content: promptString,
       },
     ],
-    model: "llama3-8b-8192",
+    model: "llama3-70B-8192",
   });
+  console.log(promptString);
   return NextResponse.json({ generator });
 }
