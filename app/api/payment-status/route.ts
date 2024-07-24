8c56d5d31db6271a3c06a8129f4ad4a7b9e0c32d
kaushal1717 <kpadaliya1717@gmail.com>
2024-07-24 11:31:28 +0530

    Revert "added env"
    This reverts commit e5bbd886539067d5b67ddc1c9d99c873ac92fcac.

--- app/api/payment-status/route.ts
+++ app/api/payment-status/route.ts
@@ -43,11 +43,11 @@ export async function POST(req: any, res: NextResponse) {
       const user = await onPaymentSuccess("premium", userId);
       console.log(user);
     }
-    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/success`, {
+    return NextResponse.redirect("http://localhost:3000/success", {
       status: 301,
     });
   } else
-    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/failure`, {
+    return NextResponse.redirect("http://localhost:3000/failure", {
       // a 301 status is required to redirect from a POST to a GET route
       status: 301,
     });
