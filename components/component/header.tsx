5de928455ef5b7b24ab66fb439f4eeb292389168
ratishjain <ratishjain6@gmail.com>
2024-07-24 18:20:26 +0530

    mobile navigation and responsiveness

--- components/component/header.tsx
+++ components/component/header.tsx
@@ -14,6 +14,15 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
 
 const Header = () => {
   const { data } = useSession();
@@ -31,7 +40,7 @@ const Header = () => {
           <MailboxIcon className="h-6 w-6" />
           <span className="ml-4 text-md font-medium">Coldmail.io</span>
         </Link>
-        <nav className="ml-auto flex gap-4 sm:gap-6 items-center">
+        <nav className="ml-auto hidden sm:flex gap-4 sm:gap-6 items-center">
           <Link
             className="text-md font-medium hover:underline underline-offset-4"
             href="/templates"
@@ -82,6 +91,74 @@ const Header = () => {
             </DropdownMenu>
           )}
         </nav>
+        <div className="w-full flex  justify-end sm:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <svg
+                className="block h-4 w-4 fill-current"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Mobile menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+              </svg>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Coldmail.io</SheetTitle>
+              </SheetHeader>
+              <nav className="flex  flex-col-reverse gap-4 sm:gap-6 items-start mt-6">
+                {data && (
+                  <Button onClick={onGoogleSignOut} className="w-full">
+                    Logout{" "}
+                    <span className="ml-3">
+                      <LogOutIcon size={20} />
+                    </span>
+                  </Button>
+                )}
+                <Link
+                  className="text-md font-medium hover:underline underline-offset-4"
+                  href="/templates"
+                >
+                  Templates
+                </Link>
+                <Link
+                  className="text-md font-medium hover:underline underline-offset-4"
+                  href="/pricing"
+                >
+                  Pricing
+                </Link>
+
+                {!data ? (
+                  <Button
+                    className="flex items-center gap-2 w-full"
+                    onClick={onGoogleSignIn}
+                  >
+                    <User2 size={16} />
+                    Login
+                  </Button>
+                ) : (
+                  <div className="  py-1 rounded-md w-full flex flex-col items-center justify-center gap-1">
+                    <Link href="/profile">
+                      <Image
+                        className="rounded-full"
+                        src={data.user?.image || ""}
+                        height={50}
+                        width={50}
+                        alt="user"
+                      />
+                    </Link>
+                    <span className="text-xl font-bold">{data.user?.name}</span>
+                    <span className="text-slate-400 text-sm">
+                      {data.user?.email}
+                    </span>
+                    <Separator className="mt-3" />
+                  </div>
+                )}
+              </nav>
+            </SheetContent>
+          </Sheet>
+        </div>
       </header>
     </div>
   );
